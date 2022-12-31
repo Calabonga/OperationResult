93e83a3258045a7c1851af374fb1de4dee027d19
Calabonga <dev@calabonga.net>
2022-12-31 11:36:58 +0700

    docs updated

--- src/Calabonga.OperationResults/IHaveDataObject.cs
+++ src/Calabonga.OperationResults/IHaveDataObject.cs
@@ -5,5 +5,9 @@
 /// </summary>
 public interface IHaveDataObject
 {
+    /// <summary>
+    /// Adds data to your operation result some type.
+    /// </summary>
+    /// <param name="data"></param>
     void AddData(object data);
 }
